c620e550cbd6a9d0b9ee85cb79f30b030b24e40c
sankafj <73643138+sankafj@users.noreply.github.com>
2021-09-09 12:27:43 +0200

    Update Auto.link.short.js

--- Auto.link.short.js
+++ Auto.link.short.js
@@ -1,4 +1,12 @@
-
+  
+function extractDomain(url) {
+	var hostname;
+	if (url.indexOf("://") > -1) {hostname = url.split('/')[2];}
+	else {hostname = url.split('/')[0];}
+	hostname = hostname.split(':')[0];
+	hostname = hostname.split('?')[0];
+	return hostname;
+}
 function exception(){
 	var exception = new Array();	
 	setting.exceptionurl = setting.exceptionurl;
